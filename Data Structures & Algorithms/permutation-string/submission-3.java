48e59ea154c0311ddbc9148805248aaf0947a52a
smriti <53113061+smriti1308@users.noreply.github.com>
2026-07-01 22:55:57 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        
+        if(s1.length()==s2.length() && s1.equals(s2)){
+            return true;
+        }
+
+
+        int[] freqs1 = new int[26];
+        for(int i = 0 ; i< s1.length();i++){
+            freqs1[s1.charAt(i) - 'a']++;
+
+        }
+
+        for(int i = 0 ; i< s2.length();i++){
+            int[] freqs2 = new int[26];
+            int R = i;
+            for(int j = 0;j<s1.length() && R<s2.length();j++){
+
+                freqs2[s2.charAt(R) - 'a']++;
+
+                R++;
+
+            }
+            if(Arrays.equals(freqs1,freqs2))
+                return true;
+
+        }
+
+
+        return false;
+
+
+
+    }
+}
